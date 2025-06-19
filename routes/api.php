fd2e783768f932b026a9b22974addc68923df409
José López <Josevld412@gmail.com>
2025-06-19 04:26:40 -0400

    Rutas de los productos

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserLanguageController;
 use App\Http\Middleware\AuthApiMiddleware;
 use App\Http\Middleware\SetUserLocale;
@@ -14,8 +15,17 @@ Route::middleware([SetUserLocale::class, AuthApiMiddleware::class])->group(funct
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
 
-    
-
     Route::get('/user/language', [UserLanguageController::class, 'getLanguage']);
     Route::put('/user/language', [UserLanguageController::class, 'updateLanguage']);
+
+     Route::prefix('products')->group(function () {
+        Route::get('/', [ProductController::class, 'index']);         
+        Route::post('/', [ProductController::class, 'store']);           
+        Route::get('/{id}', [ProductController::class, 'show']);       
+        Route::put('/{id}', [ProductController::class, 'update']);
+        Route::delete('/{id}', [ProductController::class, 'destroy']);
+
+        Route::get('/{id}/prices', [ProductController::class, 'prices']);
+        Route::post('/{id}/prices', [ProductController::class, 'addPrice']);
+    });
 });
